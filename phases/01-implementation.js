b06da0f20018af16aa28f2c3ebd42c29ce35d08d
kevin ocansey <kevinocansey11@gmail.com>
2023-01-13 20:11:28 +0000

    completed

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -1,16 +1,20 @@
 class KeyValuePair {
-  constructor(key, value) {
+  constructor(key, value, next = null) {
     this.key = key;
     this.value = value;
-    this.next = null;
+    this.next = next;
   }
 }
 
 class HashTable { // get O(1), set O(1), deleteKey O(1)
-
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity)
+    this.data.fill(null, 0, numBuckets)
   }
 
   hash(key) {
@@ -31,21 +35,99 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
-  }
+    let index = this.hashMod(key)
+    let newNode  =  new KeyValuePair(key,value)
 
+    if(this.data[index] === null){
+      this.data[index] = newNode
+      this.count++
+    }
+    else{
+      // first traverse the list to see if key values repeat themselves
+      let keyCheck = this.data[index]
+      let replaced = false
+
+      while(keyCheck){
+        if(replaced){
+          break
+        }
+        if(keyCheck.key == key && keyCheck.value !== value){
+          replaced = true
+          return keyCheck.value = value
+        }
+        else{
+          keyCheck = keyCheck.next
+        }
+      }
+
+      // handles hashCollision if replaced never results to true
+      let head =  this.data[index]
+      this.data[index] = newNode
+      this.data[index].next = head
+      this.count++
+    }
+  }
 
   read(key) {
     // Your code here
+    let index = this.hashMod(key)
+    let head =  this.data[index]
+
+    while(head){
+      if(head.key == key){
+        return head.value
+      }
+      head = head.next
+    }
+    return undefined
   }
 
 
   resize() {
     // Your code here
+    this.capacity *= 2
+    let oldArr = []
+    this.count = 0
+
+    // put old values in new oldArr to keep them for a while
+    for(let i = 0; i < this.data.length; i++){
+      oldArr.push(this.data[i])
+    }
+
+    //create new resized Array
+    this.data = new Array(this.capacity)
+
+    // for loop to reinsert last nodes of arrays back into the newArray
+    for(let i = 0; i < oldArr.length; i++){
+      let head = oldArr[i]
+      // go to last node of the head
+      while(head.next !== null){
+        if(head.next.next == null){
+          this.insert(head.next.key, head.next.value)
+          head.next = null
+        }
+        else{
+          head = head.next
+        }
+      }
+      // if only one key in head, insert head.key and .value
+      this.insert(head.key, head.value)
+    }
   }
 
 
   delete(key) {
     // Your code here
+    let index = this.hashMod(key)
+    let head =  this.data[index]
+
+    while(head.key !== key){
+      head = head.next
+    }
+    head.value = undefined
+    this.count--
+    return 'Key not found'
+    // the problem is, if you set head to null, you fuck up the next values
   }
 }
 
--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,25 +1,83 @@
 function anagrams(str1, str2) {
   // Your code here
-}
+  let set1 =  new Set(str1)
+  let set2 =  new Set(str2)
+  if(set1.size !== set2.size){
+    return false
+  }
 
+ for(let i = 0; i < set1.size; i++){
+   let value = str1[i]
+   if(!(set2.has(value))) {
+    return false
+   }
+  }
+  return true
+}
 
 function commonElements(arr1, arr2) {
   // Your code here
-}
+  let set1 =  new Set(arr1)
+  let set2 =  new Set(arr2)
+  let newArr = []
 
+  for(let i = 0; i < set1.size; i++){
+   let value = arr1[i]
+    if((set2.has(value))) {
+      newArr.push(value)
+    }
+  }
+  return newArr
+
+}
 
 function duplicate(arr) {
   // Your code here
-}
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
 
+    if (obj[num]) {
+      return num;
+    } else {
+      obj[num] = 'available';
+    }
+  }
+}
 
 function twoSum(nums, target) {
   // Your code here
+  let set1 = new Set(nums)
+  for(let i = 0; i < nums.length;i++ ){
+    let value =  target - nums[i]
+
+    if(set1.has(value) && value !==nums[i]){
+      return true
+    }
+ }
+
+ return false
 }
 
 
 function wordPattern(pattern, strings) {
   // Your code here
+  const obj = {};
+  const assigned = new Set()
+
+  for (let i = 0; i < strings.length; i++) {
+    let str = strings[i];
+    let key = pattern[i];
+
+    if(!obj[key] && !assigned.has(str) ) {
+      obj[key] = str;
+      assigned.add(str);
+    } else if (obj[key] !== str) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 
